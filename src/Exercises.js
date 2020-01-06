2ebbccdc30515fc3792a0c60bc5756ae8dfff766
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-06 13:37:33 +0100

    added exercises for every week

---
+++ src/Exercises.js
@@ -0,0 +1,13 @@
+let exercises = {
+  1:{
+    1:"j1u1",
+    2:"j1u2",
+    3:"j1u3"
+  },
+  2:{
+    1:"j2u1",
+    2:"j2u2",
+    3:"j2u3"
+  }
+}
+export default exercises;
--- src/WeekComponent.js
+++ src/WeekComponent.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
+import exercises from "./Exercises";
 
 function WeekComponent(props) {
-
-  const wStyle = {
-    width: 110
-  }
 
   const navStyle = {
     color: 'black'
   };
 
+  let weekNr = props.match.params.weekNr[0];
+  let dayNr = props.match.params.dayNr[0];
   return (
-    <div style={wStyle}>
-       Ju jeni te java  {props.match.params.weekNr[0]} dita {props.match.params.dayNr[0]}
+    <div>
+       <span>{exercises[weekNr][dayNr]}</span>
        <Link className="link-style" style={navStyle} to='/'>
           <div>Back to home</div>
         </Link>
