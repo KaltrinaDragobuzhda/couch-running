7057d24da5815acae07a4f946aa4a3de1f43cc73
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-06 14:53:40 +0100

    added exercises for every week

--- src/Exercises.js
+++ src/Exercises.js
@@ -23,11 +23,26 @@ let exercises = {
     1:"5	Brisk five-minute warmup walk, then:Jog 1/2 mile (or 5 minutes). Walk 1/4 mile (or 3 minutes). Jog 1/2 mite (or 5 minutes). •	Walk 1/4 mile (or 3 minutes) •	Jog 1/2 mite (or 5 minutes)	Brisk five-minute warmup walk, then:Jog 3/4 mile (or 8 minutes) Walk 1/2 mile (or 5 minutes) Jog 3/4 mile (or 8 minutes)	Brisk five-minute warmup walk, then jog two miles (or 20 minutes) with no walking.",
     2:"Brisk five-minute warmup walk, then: Jog 3/4 mile (or 8 minutes) Walk 1/2 mile (or 5 minutes) Jog 3/4 mile (or 8 minutes)",
     3:"Brisk five-minute warmup walk, then jog two miles (or 20 minutes) with no walking."
-  }
+  },
   6: {
-    1:
-    2:
-    3:
+    1:"Brisk five-minute warmup walk, then • Jog 1/2 mile(or 5 minutes) Walk 1/4 mile (or 3 minutes) • Jog 3/4 mile (or 8 minutes) Walk 1/4 mile (or 3 minutes) • Jog 1/2 mile (or 5 minutes)",
+    2:"Brisk five-minute warmup walk, then: Jog 1 mile minutes) Walk 1/4 mile (or 3 minutes). Jog 1 mile (or 10 minutes)",
+    3:"Brisk five-minute warmup walk, then jog 2-1/4 mile or 22 minutes with no walking."
+  },
+  7: {
+    1:"Brisk five-minute warmup walk, then jog 2.5 miles (or 25 minutes)",
+    2:"Brisk five-minute warmup walk, then jog 2.5 miles (or 25 minutes)",
+    3:"Brisk five-minute warmup walk, then jog 2.5 miles (or 25 minutes)"
+  },
+  8: {
+    1:"Brisk five-minute warmup walk, then jog 2.75 miles (or 28 minutes).",
+    2:"Brisk five-minute warmup walk, then jog 2.75 miles (or 28 minutes).",
+    3:"Brisk five-minute warmup walk, then jog 2.75 miles (or 28 minutes)."
+  },
+  9: {
+    1:"Brisk five-minute warmup walk, then jog 3 miles (or 30 minutes).",
+    2:"Brisk five-minute warmup walk, then jog 3 miles (or 30 minutes).",
+    3:"The final workout! Congratulations! Brisk five-minute warmup walk, then jog 3 miles (or 30 minutes)."
   }
 }
 export default exercises;
