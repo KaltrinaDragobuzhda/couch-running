0c1045956623ae6f9f827f663c3fed6967772159
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-06 14:35:08 +0100

    added some of the exercises

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,13 +1,33 @@
 let exercises = {
   1:{
-    1:"j1u1",
-    2:"j1u2",
-    3:"j1u3"
+    1:"Brisk five-minute warmup walk.Then alternate 60 seconds of jogging and 90 seconds of walking for a total of 20 minutes.",
+    2:"Brisk five-minute warmup walk.Then alternate 60 seconds of jogging and 90 seconds of walking for a total of 20 minutes.",
+    3:"Brisk five-minute warmup walk.Then alternate 60 seconds of jogging and 90 seconds of walking for a total of 20 minutes."
   },
   2:{
-    1:"j2u1",
-    2:"j2u2",
-    3:"j2u3"
+    1:"Brisk five-minute warmup walk.Then alternate 90 seconds of jogging and two minutes of walking for a total of 20 minutes.",
+    2:"Brisk five-minute warmup walk.Then alternate 90 seconds of jogging and two minutes of walking for a total of 20 minutes.",
+    3:"Brisk five-minute warmup walk.Then alternate 90 seconds of jogging and two minutes of walking for a total of 20 minutes."
+  },
+  3:{
+    1:"Brisk five-minute warmup walk, then do two repetitions of the following:Jog 200 yards (or 90 seconds) Walk 200 yards (or 90 seconds) Jog 400 yards (or 3 minutes) Walk 400 yards (or three minutes)",
+    2:"Brisk five-minute warmup walk, then do two repetitions of the following:Jog 200 yards (or 90 seconds) Walk 200 yards (or 90 seconds) Jog 400 yards (or 3 minutes) Walk 400 yards (or three minutes)",
+    3:"Brisk five-minute warmup walk, then do two repetitions of the following:Jog 200 yards (or 90 seconds) Walk 200 yards (or 90 seconds) Jog 400 yards (or 3 minutes) Walk 400 yards (or three minutes)"
+  },
+  4:{
+    1:"Brisk five-minute warmup walk, then:•	Jog 1/4 mile (or 3 minutes) Walk 1/8 mile (or 90 seconds) •	Jog 1/2 mile (or 5 minutes) Walk 1/4 mile (or 2-1/2 minutes) •Jog 1/4 mile (or 3 minutes) •Walk 1/8 mile (or 90 seconds) •Jog 1/2 mile (or 5 minutes)",
+    2:"Brisk five-minute warmup walk, then:•	Jog 1/4 mile (or 3 minutes) Walk 1/8 mile (or 90 seconds) •	Jog 1/2 mile (or 5 minutes) Walk 1/4 mile (or 2-1/2 minutes) •Jog 1/4 mile (or 3 minutes) •Walk 1/8 mile (or 90 seconds) •Jog 1/2 mile (or 5 minutes)",
+    3:"Brisk five-minute warmup walk, then:•	Jog 1/4 mile (or 3 minutes) Walk 1/8 mile (or 90 seconds) •	Jog 1/2 mile (or 5 minutes) Walk 1/4 mile (or 2-1/2 minutes) •Jog 1/4 mile (or 3 minutes) •Walk 1/8 mile (or 90 seconds) •Jog 1/2 mile (or 5 minutes)"
+  },
+  5: {
+    1:"5	Brisk five-minute warmup walk, then:Jog 1/2 mile (or 5 minutes). Walk 1/4 mile (or 3 minutes). Jog 1/2 mite (or 5 minutes). •	Walk 1/4 mile (or 3 minutes) •	Jog 1/2 mite (or 5 minutes)	Brisk five-minute warmup walk, then:Jog 3/4 mile (or 8 minutes) Walk 1/2 mile (or 5 minutes) Jog 3/4 mile (or 8 minutes)	Brisk five-minute warmup walk, then jog two miles (or 20 minutes) with no walking.",
+    2:"Brisk five-minute warmup walk, then: Jog 3/4 mile (or 8 minutes) Walk 1/2 mile (or 5 minutes) Jog 3/4 mile (or 8 minutes)",
+    3:"Brisk five-minute warmup walk, then jog two miles (or 20 minutes) with no walking."
+  }
+  6: {
+    1:
+    2:
+    3:
   }
 }
 export default exercises;
