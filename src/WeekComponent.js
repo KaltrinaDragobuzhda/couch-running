3b481dd4b2e69f0858aa760a3f231df71c13e058
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-08 17:12:44 +0100

    added setInterval

--- src/WeekComponent.js
+++ src/WeekComponent.js
@@ -3,33 +3,36 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import exercises from "./Exercises";
 
-function WeekComponent(props) {
+class Ujk extends React.Component {
 
-  const navStyle = {
-    color: 'black'
-  };
-
-  let weekNr = props.match.params.weekNr[0];
-  let dayNr = props.match.params.dayNr[0];
-
-  function shoot() {
-    console.log('listened');
+  constructor(props) {
+    super(props);
+    this.stopwatch = this.stopwatch.bind(this);
+    this.state = {
+      weekNr: props.match.params.weekNr[0],
+      dayNr: props.match.params.dayNr[0],
+      seconds: 0
+    }
   }
   
-  function stopwatch(){
-   let id = setInterval(() => {
-     console.log("hello world");
+  stopwatch() {
+   setInterval(() => {
+     this.setState({
+      seconds: this.state.seconds+1
+     });
    },1000);
   }
-  return (
-    <div>
-      <button onClick={stopwatch}>Take the shot!</button>
-       <span>{exercises[weekNr][dayNr]}</span>
-       <Link className="link-style" style={navStyle} to='/'>
+  render() {
+  return <div>
+      <button onClick={this.stopwatch}>Take the shot!</button>
+       <span>{exercises[this.state.weekNr][this.state.dayNr]}</span>
+       <div>{this.state.seconds}</div>
+       <Link className="link-style" style={this.navStyle} to='/'>
           <div>Back to home</div>
         </Link>
-    </div>
-  );
+    </div>;
+  }
 }
 
-export default WeekComponent;
+
+export default Ujk;
