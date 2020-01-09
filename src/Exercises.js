6f372b102610777e9619a654af73d7e6cbc2b2d0
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-09 14:02:00 +0100

    commit

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,4 +1,3 @@
-import React from 'react';
 
 let exercises = {
   1:{
--- src/WeekComponent.js
+++ src/WeekComponent.js
@@ -14,22 +14,27 @@ class Ujk extends React.Component {
       seconds: 0
     }
   }
-  
+
   stopwatch() {
-   setInterval(() => {
-     this.setState({
-      seconds: this.state.seconds+1
-     });
-   },1000);
+   var idVar = setInterval(() => {
+      this.setState({
+        seconds: this.state.seconds + 1
+      });
+      if (this.state.seconds === 2) {
+        this.seconds = 0;
+        clearInterval(idVar)
+      }
+    }, 1000);
   }
+  
   render() {
-  return <div>
+    return <div>
       <button onClick={this.stopwatch}>Start</button> <br />
-       <span>{exercises[this.state.weekNr][this.state.dayNr]}</span><br />
-       <div>{this.state.seconds}</div> <br />
-       <Link className="link-style" style={this.navStyle} to='/'>
-          <div>Back to home</div>
-        </Link>
+      <span>{exercises[this.state.weekNr][this.state.dayNr]}</span><br />
+      <div>{this.state.seconds}</div> <br />
+      <Link className="link-style" style={this.navStyle} to='/'>
+        <div>Back to home</div>
+      </Link>
     </div>;
   }
 }
