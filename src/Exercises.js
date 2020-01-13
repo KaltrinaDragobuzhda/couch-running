9d437a3f00c7800bf302be919ee9767b306acc5d
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-01-13 17:08:38 +0100

    Added minutes to the WeekComponent, fixed seconds not to take 1second more and added TotalSeconds

--- src/Exercises.js
+++ src/Exercises.js
@@ -3,7 +3,7 @@ let exercises = {
   1: {
     1: {
       human: "Brisk five-minute warmup walk.Then alternate 60 seconds of jogging and 90 seconds of walking for a total of 20 minutes.",
-      machine: [3, 6, 9]
+      machine: [3, 3, 3]
     },
     2: {
       human: "Brisk five-minute warmup walk.Then alternate 60 seconds of jogging and 90 seconds of walking for a total of 20 minutes.",
--- src/WeekComponent/WeekComponent.js
+++ src/WeekComponent/WeekComponent.js
@@ -3,19 +3,26 @@ import './WeekComponent.css';
 import { Link } from 'react-router-dom';
 import exercises from "../Exercises";
 
-class Ujk extends React.Component {
+class WeekComponent extends React.Component {
 
   constructor(props) {
     super(props);
     this.start = this.start.bind(this);
     this.stopwatch = this.stopwatch.bind(this);
+    this.intervalId = null;
+    this.stepIndex = 0;
+    this.weekNr = props.match.params.weekNr[0];
+    this.dayNr = props.match.params.dayNr[0];
     this.state = {
-      weekNr: props.match.params.weekNr[0],
-      dayNr: props.match.params.dayNr[0],
       seconds: 0,
       totalSeconds: 0,
-      action: "idle",
-      stepIndex: 0
+      action: "idle"
+    }
+  }
+
+  componentWillUnmount() {
+    if(this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 
@@ -25,39 +32,43 @@ class Ujk extends React.Component {
     }, this.stopwatch);
   }
 
+  toMins(seconds){
+    return Math.floor(seconds/60) + ":" + (seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60);
+  }
+
   stopwatch() {
-   this.intervalId = setInterval(() => {
-     if(this.state.stepIndex === exercises[this.state.weekNr][this.state.dayNr].machine.length) {
-      clearInterval(this.intervalId);
-      this.setState({
-        action:"ended"
-      });
-     }
-     else {
-      if(this.state.seconds === exercises[this.state.weekNr][this.state.dayNr].machine[this.state.stepIndex]) {
-        this.setState({
-          seconds: 1,
-          totalSeconds: this.state.totalSeconds+1,
-          stepIndex: this.state.stepIndex+1,
-          action: this.state.action==="run"?"walk":"run"
-        });
-       }
-       else {
+    this.intervalId = setInterval(() => {
+      if(this.state.seconds === exercises[this.weekNr][this.dayNr].machine[this.stepIndex]) {
+        if(this.state.stepIndex === exercises[this.weekNr][this.dayNr].machine.length - 1) {
+          clearInterval(this.intervalId);
+          this.setState({
+            action:"ended"
+          });
+        }
+        else {
+          this.stepIndex++;
+          this.setState({
+            seconds: 1,
+            totalSeconds: this.state.totalSeconds+1,
+            action: this.state.action==="run"?"walk":"run"
+          });
+        }
+      }
+      else {
         this.setState({
           seconds: this.state.seconds + 1,
           totalSeconds: this.state.totalSeconds+1
         });
-       }
-     }
+      }
     }, 1000);
   }
   
   render() {
     return <div className="week-component-container">
       <button className="start-button" onClick={this.start}>Start</button> 
-      <span className="show-human-exercises">{exercises[this.state.weekNr][this.state.dayNr].human}</span>
-      <div className="counting-seconds">{this.state.seconds}</div> 
-      <div className="counting-seconds">{this.state.totalSeconds}</div> 
+      <span className="show-human-exercises">{exercises[this.weekNr][this.dayNr].human}</span>
+      <div className="counting-seconds">{this.toMins(this.state.seconds)}</div> 
+      <div className="counting-seconds">{this.toMins(this.state.totalSeconds)}</div> 
       <div className="state-action">{this.state.action}</div> 
       <Link className="link-style" style={this.navStyle} to='/'>
         <div>Back to home</div>
@@ -66,4 +77,4 @@ class Ujk extends React.Component {
   }
 }
 
-export default Ujk;
+export default WeekComponent;
