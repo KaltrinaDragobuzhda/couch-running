ff8d3683380cbbd0f7d447d4680d8c6734d76c9e
kaltrinadragobuzhda <kaltrina.dragobuzhda@gmail.com>
2020-02-24 15:36:37 +0100

    added build in docs directory

---
+++ docs/build/precache-manifest.fafa83955d7c04b364a448bd5eec42a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38e6672fcd7f2e96b1373cbfd3a2bcf",
+    "url": "/KaltrinaDragobuzhda/couch-running/index.html"
+  },
+  {
+    "revision": "2d94efd70fbeb4665306",
+    "url": "/KaltrinaDragobuzhda/couch-running/static/css/main.78bf6210.chunk.css"
+  },
+  {
+    "revision": "6b5f9a13a3dd54e99885",
+    "url": "/KaltrinaDragobuzhda/couch-running/static/js/2.d100f19e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/KaltrinaDragobuzhda/couch-running/static/js/2.d100f19e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d94efd70fbeb4665306",
+    "url": "/KaltrinaDragobuzhda/couch-running/static/js/main.076909d1.chunk.js"
+  },
+  {
+    "revision": "a5a01f4bb4cc309ec37f",
+    "url": "/KaltrinaDragobuzhda/couch-running/static/js/runtime-main.7528791f.js"
+  }
+]);
